331302838ea16728c289f3fa3d4208e5106236f5
Stevandean <322310017@student.machung.ac.id>
2026-04-28 07:11:23 +0700

    upd: fix build error

--- app/api/report/[id]/route.ts
+++ app/api/report/[id]/route.ts
@@ -1,34 +1,32 @@
 import { supabase } from "@/lib/superbase";
+import { NextRequest } from "next/server";
 
 export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> }
 ) {
-  try {
-    const { id } = await params;
+  const { id } = await context.params;
 
-    const { data: report, error } = await supabase
-      .from("reports")
-      .select("*")
-      .eq("id", id)
-      .single();
+  const { data: report, error } = await supabase
+    .from("reports")
+    .select("*")
+    .eq("id", id)
+    .single();
 
-    if (error) throw error;
-
-    const { data: images } = await supabase
-      .from("report_images")
-      .select("image_url")
-      .eq("report_id", id);
-
-    return Response.json({
-      ...report,
-      images: images?.map((img) => img.image_url) || [],
-    });
-
-  } catch (err: any) {
+  if (error || !report) {
     return Response.json(
       { error: "Data tidak ditemukan" },
       { status: 404 }
     );
   }
+
+  const { data: images } = await supabase
+    .from("report_images")
+    .select("image_url")
+    .eq("report_id", id);
+
+  return Response.json({
+    ...report,
+    images: images?.map((img) => img.image_url) || [],
+  });
 }
